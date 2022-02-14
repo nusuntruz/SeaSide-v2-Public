105a6e85786b62478ad60a0d98b569265ae80e2d
nusuntruz <75524995+nusuntruz@users.noreply.github.com>
2022-02-14 16:15:32 +0200

    Update logindata.lua

--- logindata.lua
+++ logindata.lua
@@ -2,7 +2,22 @@ local username_true = {}
 local username = globals.get_username()
 username_true['rings1337'] = true
 username_true['Ruzule'] = true
-
+username_true['ItapingU'] = true
+username_true['Remiii'] = true
+username_true['DariusQIZ'] = true
+username_true['Prync'] = true
+username_true['Cabrons'] = true
+username_true['JNHVH'] = true
+username_true['thunderzeraa'] = true
+username_true['Jdarey'] = true
+username_true['xFr3sH'] = true
+username_true['K69'] = true
+username_true['Mikuu'] = true
+username_true['Retadz'] = true
+username_true['Ignacy'] = true
+username_true['DeadEye'] = true
+username_true['Bearlyeight75'] = true
+username_true['Jason08910'] = true
 
 local function sendtrue()
     if username_true[username] == true then
